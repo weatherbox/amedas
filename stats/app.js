5bbd3002b2c33d044fdeda74331d0c70bf8cb577
tattii <yuta.tatti@gmail.com>
2017-04-10 19:48:35 +0900

    load data.csv and amedas-point.json

--- stats/app.js
+++ stats/app.js
@@ -4,7 +4,8 @@ import {render} from 'react-dom';
 import MapGL from 'react-map-gl';
 import DeckGLOverlay from './deckgl-overlay.js';
 
-import {csv as requestCsv} from 'd3-request';
+import {request, json as requestJSON} from 'd3-request';
+import {csvParseRows} from 'd3-dsv';
 
 // Set your mapbox token here
 const MAPBOX_TOKEN = 'pk.eyJ1IjoidGF0dGlpIiwiYSI6ImNqMWFrZ3ZncjAwNmQzM3BmazRtNngxam8ifQ.DNMc6j7E4Gh7UkUAaEAPxA'; // eslint-disable-line
@@ -22,13 +23,34 @@ class Root extends Component {
       data: null
     };
 
-    requestCsv('./data/heatmap-data.csv', (error, response) => {
+    requestJSON('amedas-point.json', (error, res) => {
       if (!error) {
-        const data = response.map(d => ([Number(d.lng), Number(d.lat)]));
-        this.setState({data});
+        let point = res;
+        this._loadCSV('pre24h00_rct.csv', point);
       }
     });
   }
+
+  _loadCSV(url, point) {
+    let time;
+    request(url, (xhr) => {
+      let data = csvParseRows(xhr.responseText, (d, i) => {
+        if (i == 0) return null; // header
+        if (i == 1) time = d.slice(4, 9);
+
+        let id = d[0];
+        return [
+          id,
+          +d[9], // value
+          point[id].lat,
+          point[id].lon
+        ];
+      });
+      console.log(time);
+      console.table(data);
+      //this.setState({data});
+    });
+  } 
 
   componentDidMount() {
     window.addEventListener('resize', this._resize.bind(this));
