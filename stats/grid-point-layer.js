d01e827353992c277a968e9ca1020bd1c774e9da
tattii <yuta.tatti@gmail.com>
2017-04-10 20:20:32 +0900

    copy grid-aggregator.js for overwrite pointToDensityGridData

--- stats/grid-point-layer.js
+++ stats/grid-point-layer.js
@@ -1,8 +1,23 @@
 /* global window */
 import DeckGL, {GridLayer} from 'deck.gl';
+import {pointToDensityGridData} from './grid-aggregator';
+
+function _needsReProjectPoints(oldProps, props) {
+  return oldProps.cellSize !== props.cellSize;
+}
 
 export default class GridPointLayer extends GridLayer {
 
+  updateState({oldProps, props, changeFlags}) {
+    if (changeFlags.dataChanged || _needsReProjectPoints(oldProps, props)) {
+      const {data, cellSize, getPosition} = this.props;
+
+      const {layerData, countRange} =
+        pointToDensityGridData(data, cellSize, getPosition);
+
+      Object.assign(this.state, {layerData, countRange});
+    }
+  }
 
 };
 
