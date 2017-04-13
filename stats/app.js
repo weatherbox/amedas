5cf4274f4020a4020c49262ea806aee77191485e
tattii <yuta.tatti@gmail.com>
2017-04-13 19:16:33 +0900

    add time select button

--- stats/app.js
+++ stats/app.js
@@ -1,7 +1,7 @@
 /* global window,document */
 import React, {Component} from 'react';
 import {render} from 'react-dom';
-import { Header } from 'semantic-ui-react';
+import { Header, Button } from 'semantic-ui-react';
 import Map from './map.js';
 
 const styles = {
@@ -22,6 +22,16 @@ const styles = {
     color: '#fff',
     zIndex: 1000,
     fontWeight: 'normal'
+  },
+  buttongroup: {
+    position: 'absolute',
+    bottom: 15,
+    left: 15,
+    zIndex: 1000,
+  },
+  button: {
+    color: "#fff",
+    backgroundColor: "rgba(0, 0,0, 0.8)" 
   }
 };
 
@@ -33,6 +43,13 @@ class Root extends Component {
         <Header as='h1' style={styles.title1}>24</Header>
         <Header as='h3' style={styles.title2}>時間降水量</Header>
         <Map />
+        <Button.Group size='tiny' color='black' style={styles.buttongroup}>
+          <Button style={styles.button}>1h</Button>
+          <Button style={styles.button}>3h</Button>
+          <Button style={styles.button}>24h</Button>
+          <Button style={styles.button}>48h</Button>
+          <Button style={styles.button}>72h</Button>
+        </Button.Group>
       </div>
     );
   }
